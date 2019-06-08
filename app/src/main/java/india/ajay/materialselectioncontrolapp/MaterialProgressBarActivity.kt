4dd505f6b8a76039b1fa30fbf686541b37a1ca7e
Ajay Prajapati <ajay4android@gmail.com>
2019-06-08 20:52:57 +0530

    added linear and circular progress bar

--- app/src/main/java/india/ajay/materialselectioncontrolapp/MaterialProgressBarActivity.kt
+++ app/src/main/java/india/ajay/materialselectioncontrolapp/MaterialProgressBarActivity.kt
@@ -10,9 +10,11 @@ import java.util.*
 class MaterialProgressBarActivity : AppCompatActivity() {
 
     private var progressBar: ProgressBar? = null
+    private var linearProgressBar: ProgressBar? = null
     private var handler: Handler? = null
     private var runnable: Runnable? = null
     private var timer: Timer? = null
+    private var counter: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,12 +23,52 @@ class MaterialProgressBarActivity : AppCompatActivity() {
     }
 
     private fun setupView(){
+        showProgressBarCircular()
+        showProgressBarLinear()
+    }
+
+    private fun showProgressBarCircular(){
         handler = Handler()
         runnable = Runnable {
-
+            timer?.cancel()
+            progressBar?.visibility = View.GONE
         }
         timer = Timer()
+        val timerTask = object: TimerTask(){
+            override fun run(){
+                handler?.post(runnable)
+            }
+        }
+        timer?.schedule(timerTask, 8000, 500)
         progressBar = findViewById(R.id.progress_bar)
         progressBar?.visibility = View.VISIBLE
     }
+
+    private fun showProgressBarLinear(){
+        handler = Handler()
+        runnable = Runnable {
+            if(++counter <= 100){
+                linearProgressBar?.progress = counter
+                linearProgressBar?.secondaryProgress = counter+10
+            } else {
+                timer?.cancel()
+            }
+
+           // linearProgressBar?.visibility = View.GONE
+        }
+        timer = Timer()
+        val timerTask = object: TimerTask(){
+            override fun run(){
+                handler?.post(runnable)
+            }
+        }
+        timer?.schedule(timerTask, 8000, 500)
+        linearProgressBar = findViewById(R.id.progress_bar_linear)
+        linearProgressBar?.progress = 0
+        linearProgressBar?.secondaryProgress = 0
+        linearProgressBar?.max = 100
+        linearProgressBar?.visibility = View.VISIBLE
+    }
+
+
 }
